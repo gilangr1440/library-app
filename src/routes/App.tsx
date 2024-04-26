e759e86f77c66b959ed5ed4448eb56bae3a3ed19
gilangr1440 <gilangr1440@gmail.com>
2024-04-26 15:09:19 +0700

    login page

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home";
 import Register from "../pages/register";
+import Login from "../pages/login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
