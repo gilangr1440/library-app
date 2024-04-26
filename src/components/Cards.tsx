bc5bfe5551502dd5d099b9123e1704991b235abb
gilangr1440 <gilangr1440@gmail.com>
2024-04-26 16:54:30 +0700

    sort-page

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -5,7 +5,7 @@ const Cards: FC<CardsProps> = ({ img, title, author }) => {
   return (
     <div className="w-56 mb-5">
       <div className="w-full h-72 shadow-lg mb-4">
-        <img src={img} className="w-full h-full object-cover" alt="cover" />
+        <img src={img} className="w-full h-full object-cover rounded-sm" alt="cover" />
       </div>
       <h1 className="text-md font-bold text-center mb-3">{title}</h1>
       <h2 className="text-sm text-gray-500 text-center">{author}</h2>
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -3,6 +3,7 @@ import Home from "../pages/home";
 import Register from "../pages/register";
 import Login from "../pages/login";
 import Detail from "../pages/detail";
+import NewReleaseBooks from "../pages/new-release-books";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/detail" element={<Detail />} />
+        <Route path="/new" element={<NewReleaseBooks />} />
       </Routes>
     </BrowserRouter>
   );
