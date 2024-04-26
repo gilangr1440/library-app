7d2f83888b4f1362c082a4e2999b1212fabaf5b4
gilangr1440 <gilangr1440@gmail.com>
2024-04-26 15:05:12 +0700

    register page

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home";
+import Register from "../pages/register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
